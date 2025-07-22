9d4b5c82688451ef74451710aea1d1cf89f68775
Le J <222042213+victoria180725@users.noreply.github.com>
2025-07-22 23:38:10 +0200

    Ajout du footer

---
+++ scripts/footer.js
@@ -0,0 +1,13 @@
+const footer = document.getElementById('footer');
+
+footer.innerHTML = `
+  <div class="footer-content">
+    <p>&copy; 2025 - Victoria. Tous droits réservés.</p>
+    <p><a href="" target="_blank" class="footer-link">Mentions légales</a></p>
+  </div>
+  <div class="footer-social">
+    <a href="https://discord.gg" target="_blank" class="footer-social-icon"><img src="icons/discord.svg" alt="Discord"></a>
+    <a href="https://github.com/victoria180725" target="_blank" class="footer-social-icon"><img src="icons/github_blank.svg" alt="GitHub"></a>
+    <a href="https://instagram.com" target="_blank" class="footer-social-icon"><img src="icons/instagram.svg" alt="Instagram"></a>
+  </div>
+`;
