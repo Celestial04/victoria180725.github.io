fff25a239b52c2aacec0162a742dfd736249c855
victoria180725 <222042213+victoria180725@users.noreply.github.com>
2025-08-17 18:05:23 +0200

    Ajout de la barre de navigation avec JS

---
+++ scripts/navbar.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menu = document.getElementById("menu");
+  menu.innerHTML = `
+    <a href="/"><img src="images/logo.svg" alt="Logo"></a>
+    <div class="links">
+      <a href="/" class="item">Accueil</a>
+      <a href="/projects" class="item">Projets</a>
+      <a href="/pictures" class="item">Galerie de photos</a>
+      <a href="contact.html" class="item">Contact</a>
+    </div>
+  `;
+});
